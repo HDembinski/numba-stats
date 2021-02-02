fe8c97a47a29d73dd189fd89fe147695889a6a80
Hans Dembinski <hans.dembinski@gmail.com>
2021-02-02 16:52:11 +0100

    fix version strings

--- tests/test_numba_stats.py
+++ tests/test_numba_stats.py
@@ -1,5 +1,20 @@
 from numba_stats import __version__
+import requests
+from pkg_resources import parse_version
+from pathlib import Path
+import toml
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_local_against_pypi_version():
+    # make sure version is up-to-date
+    pypi_versions = [
+        parse_version(v)
+        for v in requests.get("https://pypi.org/pypi/iminuit/json").json()["releases"]
+    ]
+    assert parse_version(__version__) not in pypi_versions, "pypi version exists"
+
+
+def test_poetry_version():
+    fn = Path(__file__).parent / ".." / "pyproject.toml"
+    poetry_version = toml.load(fn)["tool"]["poetry"]["version"]
+    assert __version__ == poetry_version
