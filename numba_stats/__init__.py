5d0f0e3953ccfb9db2ef2a675eb4d9d4b536f8d4
Hans Dembinski <hans.dembinski@gmail.com>
2021-02-02 16:32:04 +0100

    fixes for exponential

--- numba_stats/__init__.py
+++ numba_stats/__init__.py
@@ -1,3 +1,12 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
-from .stats import norm_pdf, norm_cdf, norm_ppf, poisson_pmf, poisson_cdf  # noqa
+from .stats import (  # noqa
+    norm_pdf,
+    norm_cdf,
+    norm_ppf,
+    poisson_pmf,
+    poisson_cdf,
+    expon_pdf,
+    expon_cdf,
+    expon_ppf,
+)
