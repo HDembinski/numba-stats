3b80f510dc15039da5351f1ae3acadbd6ee38cf4
Chris Burr <chrisburr@users.noreply.github.com>
2021-02-24 14:18:24 +0100

    Relax version pins and use single source of version number (#3)
    Co-authored-by: Hans Dembinski <hans.dembinski@gmail.com>

--- tests/test_numba_stats.py
+++ tests/test_numba_stats.py
@@ -1,8 +1,6 @@
 from numba_stats import __version__
 import requests
 from pkg_resources import parse_version
-from pathlib import Path
-import toml
 
 
 def test_local_against_pypi_version():
@@ -14,12 +12,6 @@ def test_local_against_pypi_version():
         ]
     ]
     assert parse_version(__version__) not in pypi_versions, "pypi version exists"
-
-
-def test_poetry_version():
-    fn = Path(__file__).parent / ".." / "pyproject.toml"
-    poetry_version = toml.load(fn)["tool"]["poetry"]["version"]
-    assert __version__ == poetry_version
 
 
 def test_import():
