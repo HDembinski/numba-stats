1142cb32d35c46877f57d066f6cd6d73d0cb2b38
Matthew Kenzie <matthewkenzie@users.noreply.github.com>
2021-07-20 12:52:44 +0100

    Added a continuous version of the poisson distribution (#10)
    Co-authored-by: Hans Dembinski <hans.dembinski@gmail.com>

--- numba_stats/__init__.py
+++ numba_stats/__init__.py
@@ -9,6 +9,8 @@ from .stats import (  # noqa
     norm_ppf,
     poisson_pmf,
     poisson_cdf,
+    cpoisson_pmf,
+    cpoisson_cdf,
     expon_pdf,
     expon_cdf,
     expon_ppf,
@@ -26,6 +28,7 @@ from argparse import Namespace
 uniform = Namespace(pdf=uniform_pdf, cdf=uniform_cdf, ppf=uniform_ppf)
 norm = Namespace(pdf=norm_pdf, cdf=norm_cdf, ppf=norm_ppf)
 poisson = Namespace(pmf=poisson_pmf, cdf=poisson_cdf)
+cpoisson = Namespace(pmf=cpoisson_pmf, cdf=cpoisson_cdf)
 expon = Namespace(pdf=expon_pdf, cdf=expon_cdf, ppf=expon_ppf)
 t = Namespace(pdf=t_pdf, cdf=t_cdf, ppf=t_ppf)
 voigt = Namespace(pdf=voigt_pdf)
