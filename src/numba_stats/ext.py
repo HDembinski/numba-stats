cec98bf24ed0416afb007032cfa4bcd2fcefd9d8
Hans Dembinski <hans.dembinski@gmail.com>
2021-07-26 11:16:53 +0200

    wip

---
+++ src/numba_stats/ext.py
@@ -0,0 +1,43 @@
+import numpy as np
+import numba as nb
+
+_signatures = [
+    (nb.float32[:], nb.float32[:], nb.float32[:]),
+    (nb.float64[:], nb.float64[:], nb.float64[:]),
+]
+
+
+@nb.guvectorize(_signatures, "(n),(m)->(n)")
+def _de_castlejau(z, beta, res):
+    # De Casteljau algorithm, numerically stable
+    n = len(beta)
+
+    if n == 0:
+        res[:] = 1.0
+    else:
+        betai = np.empty_like(beta)
+        for iz, zi in enumerate(z):
+            azi = 1.0 - zi
+            betai[:] = beta
+            for j in range(1, n):
+                for k in range(n - j):
+                    betai[k] = betai[k] * azi + betai[k + 1] * zi
+            res[iz] = betai[0]
+
+
+def bernstein_density(x, beta, xmin, xmax):
+    scale = 1.0 / (xmax - xmin)
+    x = np.atleast_1d(x)
+    z = (x - xmin) * scale
+    res = np.empty_like(z)
+    beta = np.atleast_1d(beta)
+    _de_castlejau(z, beta, res)
+    return res * scale / (len(beta) + 1)
+
+
+def bernstein_scaled_cdf(x, beta, xmin, xmax):
+    beta = np.atleast_1d(beta)
+    beta1 = np.empty(len(beta) + 1, dtype=beta.dtype)
+    beta1[1:] = beta
+    beta1[0] = 0
+    return bernstein_density(x, beta1, xmin, xmax)
---
+++ tests/test_ext.py
@@ -0,0 +1,28 @@
+from numba_stats import ext as nbs
+from scipy.interpolate import BPoly
+import pytest
+import numpy as np
+from scipy.integrate import quad
+
+
+@pytest.mark.parametrize("beta", [[1], [1, 1], [1, 1, 1], [1, 2, 3], [1, 3, 2]])
+def test_bernstein_density(beta):
+    x = np.linspace(1, 3)
+    # bernstein_density is normed so that beta corresponds to local density
+    got = nbs.bernstein_density(x, beta, x[0], x[-1])
+    expected = BPoly(np.array(beta)[:, np.newaxis], [x[0], x[-1]])(x) / (
+        (len(beta) + 1) * (x[-1] - x[0])
+    )
+    np.testing.assert_allclose(got, expected)
+
+
+@pytest.mark.parametrize("beta", [[1], [1, 1], [1, 1, 1], [1, 2, 3], [1, 3, 2]])
+def test_bernstein_scaled_cdf(beta):
+    x = np.linspace(1, 3)
+
+    got = nbs.bernstein_scaled_cdf(x, beta, x[0], x[-1])
+    expected = [
+        quad(lambda x: nbs.bernstein_density(x, beta, x[0], x[-1]), x[0], xi)[0]
+        for xi in x
+    ]
+    np.testing.assert_allclose(got, expected)
