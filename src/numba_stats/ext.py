b7647487968c73bc5126c3aba16b8e73848fb341
Hans Dembinski <hans.dembinski@gmail.com>
2021-07-27 17:44:15 +0200

    works, but cannot use in numba-compiled function

--- src/numba_stats/ext.py
+++ src/numba_stats/ext.py
@@ -1,19 +1,19 @@
 import numpy as np
 import numba as nb
 
+
 _signatures = [
     (nb.float32[:], nb.float32[:], nb.float32[:]),
     (nb.float64[:], nb.float64[:], nb.float64[:]),
 ]
 
 
-@nb.guvectorize(_signatures, "(n),(m)->(n)")
+@nb.njit(_signatures)
 def _de_castlejau(z, beta, res):
     # De Casteljau algorithm, numerically stable
     n = len(beta)
-
     if n == 0:
-        res[:] = 1.0
+        res[:] = np.nan
     else:
         betai = np.empty_like(beta)
         for iz, zi in enumerate(z):
@@ -25,24 +25,47 @@ def _de_castlejau(z, beta, res):
             res[iz] = betai[0]
 
 
-def bernstein_density(x, beta, xmin, xmax):
-    scale = 1.0 / (xmax - xmin)
-    x = np.atleast_1d(x)
-    z = (x - xmin) * scale
-    res = np.empty_like(z)
-    beta = np.atleast_1d(beta)
+_signatures = [
+    nb.float32[:](nb.float32[:], nb.float32[:]),
+    nb.float64[:](nb.float64[:], nb.float64[:]),
+]
+
+
+@nb.njit(_signatures)
+def _beta_int(beta, inverse_scale):
+    n = len(beta)
+    r = np.zeros(n + 1, dtype=beta.dtype)
+    for j in range(1, n + 1):
+        for k in range(j):
+            r[j] += beta[k]
+    inverse_scale /= n * (n + 1)
+    r *= inverse_scale
+    return r
+
+
+_signatures = [
+    (nb.float32[:], nb.float32[:], nb.float32[:], nb.float32[:], nb.float32[:]),
+    (nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:]),
+]
+
+
+@nb.guvectorize(_signatures, "(),(n),(),()->()")
+def bernstein_density(x, beta, xmin, xmax, res):
+    inverse_scale = np.ones_like(xmax) / (xmax - xmin)
+    z = x.copy()
+    z -= xmin
+    z *= inverse_scale
+    inverse_scale /= len(beta) + 1
+    beta_scaled = beta.copy()
+    beta_scaled *= inverse_scale
+    _de_castlejau(z, beta_scaled, res)
+
+
+@nb.guvectorize(_signatures, "(),(n),(),()->()")
+def bernstein_scaled_cdf(x, beta, xmin, xmax, res):
+    inverse_scale = np.ones_like(xmax) / (xmax - xmin)
+    z = x.copy()
+    z -= xmin
+    z *= inverse_scale
+    beta = _beta_int(beta, inverse_scale)
     _de_castlejau(z, beta, res)
-    return res * scale / (len(beta) + 1)
-
-
-def bernstein_scaled_cdf(x, beta, xmin, xmax):
-    beta = np.atleast_1d(beta)
-    beta1 = np.empty(len(beta) + 1, dtype=beta.dtype)
-    beta1[1:] = beta
-    beta1[0] = 0
-    scale = 1.0 / (xmax - xmin)
-    x = np.atleast_1d(x)
-    z = (x - xmin) * scale
-    res = np.empty_like(z)
-    _de_castlejau(z, beta1, res)
-    return res * scale / len(beta1)
--- tests/test_ext.py
+++ tests/test_ext.py
@@ -3,9 +3,12 @@ from scipy.interpolate import BPoly
 import pytest
 import numpy as np
 from scipy.integrate import quad
+import numba as nb  # noqa
 
 
-@pytest.mark.parametrize("beta", [[1], [1, 1], [1, 1, 1], [1, 2, 3], [1, 3, 2]])
+@pytest.mark.parametrize(
+    "beta", [[1.0], [1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 2.0, 3.0], [1.0, 3.0, 2.0]]
+)
 def test_bernstein_density(beta):
     x = np.linspace(1, 3)
     # bernstein_density is normed so that beta corresponds to local density
@@ -22,8 +25,17 @@ def test_bernstein_scaled_cdf(beta):
 
     got = nbs.bernstein_scaled_cdf(x, beta, x[0], x[-1])
     expected = [
-        quad(lambda xi: nbs.bernstein_density(xi, beta, x[0], x[-1]), x[0], xi)[0]
+        quad(lambda y: nbs.bernstein_density(y, beta, x[0], x[-1]), x[0], xi)[0]
         for xi in x
     ]
-    print(len(beta), expected / got)
     np.testing.assert_allclose(got, expected)
+
+
+# def test_numba_bernstein_density():
+#     x = np.linspace(1, 3)
+#
+#     @nb.njit
+#     def f(x, beta):
+#         return nbs.bernstein_density(x, beta, 0, 1)
+#
+#     f(x, [1, 2])
