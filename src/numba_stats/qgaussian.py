68224ff396133a570beaa83264dd7b8e88a78667
Hans Dembinski <hans.dembinski@gmail.com>
2021-07-30 17:35:45 +0200

    wip

---
+++ src/numba_stats/qgaussian.py
@@ -0,0 +1,44 @@
+import numba as nb
+import numpy as np
+from math import gamma
+
+
+@nb.njit
+def _qexp(x, q):
+    if q == 1:
+        return np.exp(x)
+    alpha = 1.0 - q
+    le = np.log(1.0 + alpha * x) / alpha
+    return np.exp(le)
+
+
+@nb.njit
+def _compute_cq(q):
+    sqrt_pi = np.sqrt(np.pi)
+    alpha = 1.0 - q
+    if q == 1:
+        return sqrt_pi
+    if q < 1:
+        return (
+            2.0
+            * sqrt_pi
+            * gamma(1.0 / alpha)
+            / ((3.0 - q) * np.sqrt(alpha) * gamma(0.5 * (3.0 - q) / alpha))
+        )
+    if q < 3:
+        return (
+            sqrt_pi
+            * gamma(-0.5 * (3.0 - q) / alpha)
+            / (np.sqrt(-alpha) * gamma(-1.0 / alpha))
+        )
+    return np.nan
+
+
+@nb.vectorize
+def pdf(x, q, mu, sigma):
+    inv_scale = 1.0 / sigma
+    z = (x - mu) * inv_scale
+    c_q = _compute_cq(q)
+    inv_scale /= c_q * np.sqrt(2)
+    # beta = 1/2 for equivalence with normal dist. for q = 1
+    return _qexp(-0.5 * (z ** 2), q) * inv_scale
---
+++ tests/test_qgaussian.py
@@ -0,0 +1,22 @@
+from numba_stats import qgaussian, t
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize("nu", (0.1, 1.0, 2.0))
+def test_qgaussian_pdf(nu):
+    x = np.linspace(-5, 5)
+
+    # https://en.wikipedia.org/wiki/Q-Gaussian_distribution
+    # relation to Student's t-distribution
+    q = (nu + 3) / (nu + 1)
+    beta = 1 / (3 - q)
+
+    # 2 beta x^2 = (x/sigma)^2
+    # 2 beta = 1/sigma^2
+    # sigma = 1/sqrt(2 beta)
+
+    expected = t.pdf(x, nu, 0, 1)
+    got = qgaussian.pdf(x, q, 0, 1 / np.sqrt(2 * beta))
+
+    np.testing.assert_allclose(got, expected)
