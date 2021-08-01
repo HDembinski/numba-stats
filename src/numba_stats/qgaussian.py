2a4fee235bc8b66702c8474a641402bcd4ee0bda
Hans Dembinski <hans.dembinski@gmail.com>
2021-08-01 14:54:57 +0200

    wip

--- src/numba_stats/qgaussian.py
+++ src/numba_stats/qgaussian.py
@@ -1,6 +1,8 @@
 import numba as nb
 import numpy as np
-from math import gamma
+from math import gamma, lgamma
+from ._special import hyp2f1
+from . import norm
 
 
 @nb.njit
@@ -8,28 +10,32 @@ def _qexp(x, q):
     if q == 1:
         return np.exp(x)
     alpha = 1.0 - q
-    le = np.log(1.0 + alpha * x) / alpha
+    arg = 1.0 + alpha * x
+    if arg <= 0:
+        return 0
+    le = np.log(arg) / alpha
     return np.exp(le)
 
 
 @nb.njit
 def _compute_cq(q):
-    sqrt_pi = np.sqrt(np.pi)
+    # beta = 1/2 for equivalence with normal distribution for q = 1
+    const = np.sqrt(2 * np.pi)
     alpha = 1.0 - q
     if q == 1:
-        return sqrt_pi
+        return const
     if q < 1:
         return (
             2.0
-            * sqrt_pi
-            * gamma(1.0 / alpha)
-            / ((3.0 - q) * np.sqrt(alpha) * gamma(0.5 * (3.0 - q) / alpha))
+            * const
+            / ((3.0 - q) * np.sqrt(alpha))
+            * np.exp(lgamma(1.0 / alpha) - lgamma(0.5 * (3.0 - q) / alpha))
         )
     if q < 3:
         return (
-            sqrt_pi
-            * gamma(-0.5 * (3.0 - q) / alpha)
-            / (np.sqrt(-alpha) * gamma(-1.0 / alpha))
+            const
+            / np.sqrt(-alpha)
+            * np.exp(lgamma(-0.5 * (3.0 - q) / alpha) - lgamma(-1.0 / alpha))
         )
     return np.nan
 
@@ -45,14 +51,38 @@ def pdf(x, q, mu, sigma):
     inv_scale = 1.0 / sigma
     z = (x - mu) * inv_scale
     c_q = _compute_cq(q)
-    inv_scale /= c_q * np.sqrt(2)
-    # beta = 1/2 for equivalence with normal dist. for q = 1
-    return _qexp(-0.5 * (z ** 2), q) * inv_scale
+    inv_scale /= c_q
+    # beta = 1/2 for equivalence with normal distribution for q = 1
+    if q == 1.0:
+        return np.exp(-0.5 * z ** 2) * inv_scale
+    return _qexp(-0.5 * z ** 2, q) * inv_scale
 
 
-# from sympy import *
-# from matplotlib import pyplot as plt
-#
-# x = np.linspace(-5, 5)
-#
-# plt.plot(x, pdf(x, 1, 0, 1))
+# @nb.vectorize(_signatures)
+# def cdf(x, q, mu, sigma):
+#     inv_scale = 1.0 / sigma
+#     z = (x - mu) * inv_scale
+#     c_q = _compute_cq(q)
+#     alpha = 1.0 - q
+#     return hyp2f1(0.5, -1.0 / alpha, 1.5, 0.5 * z ** 2 * alpha) / c_q + 0.5
+
+
+@nb.vectorize(_signatures)
+def cdf(x, q, mu, sigma):
+    if q < 1 or q > 2:
+        raise ValueError("q < 1 or q >= 3 are not supported")
+
+    if q == 1:
+        return norm.cdf(x, mu, sigma)
+
+    # 1 / (2 sigma^2) = 1 / (3 - q)
+    # sqrt((3 - q) / 2) = sigma
+
+    sigma *= np.sqrt(0.5 * (3.0 - q))
+    inv_scale = 1.0 / sigma
+    z = (x - mu) * inv_scale
+    nu = (3 - q) / (q - 1)
+
+    return 0.5 + z * gamma(0.5 * (nu + 1)) / np.sqrt(nu * np.pi) / gamma(
+        0.5 * nu
+    ) * hyp2f1(0.5, 0.5 * (nu + 1), 1.5, -(z ** 2) / nu)
--- tests/test_qgaussian.py
+++ tests/test_qgaussian.py
@@ -1,22 +1,54 @@
-from numba_stats import qgaussian, t
+from numba_stats import qgaussian, t, norm
 import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize("nu", (1, 3, 10))
-def test_qgaussian_pdf(nu):
-    x = np.linspace(-5, 5)
-
+def q_sigma(nu, sigma):
     # https://en.wikipedia.org/wiki/Q-Gaussian_distribution
     # relation to Student's t-distribution
-    q = (nu + 3) / (nu + 1)
-    beta = 1 / (3 - q)
 
-    # 2 beta x^2 = (x/sigma)^2
-    # 2 beta = 1/sigma^2
-    # sigma = 1/sqrt(2 beta)
+    # 1 / (2 sigma^2) = 1 / (3 - q)
+    # sqrt((3 - q) / 2) = sigma
+
+    q = (nu + 3.0) / (nu + 1.0)
+    return q, sigma * np.sqrt(0.5 * (3.0 - q))
+
+
+def test_qgaussian_pdf_vs_norm():
+    x = np.linspace(-5, 5)
+
+    expected = norm.pdf(x, 0.5, 1.2)
+    got = qgaussian.pdf(x, 1, 0.5, 1.2)
+
+    np.testing.assert_allclose(got, expected)
+
+
+def test_qgaussian_cdf_vs_norm():
+    x = np.linspace(-5, 5)
+
+    expected = norm.cdf(x, 0.5, 1.2)
+    got = qgaussian.cdf(x, 1, 0.5, 1.2)
+
+    np.testing.assert_allclose(got, expected)
+
+
+@pytest.mark.parametrize("nu", (1, 3, 10))
+def test_qgaussian_pdf_vs_t(nu):
+    x = np.linspace(-5, 5)
+    q, sigma = q_sigma(nu, 1)
 
     expected = t.pdf(x, nu, 0, 1)
-    got = qgaussian.pdf(x, q, 0, 1 / np.sqrt(2 * beta))
+    got = qgaussian.pdf(x, q, 0, sigma)
+
+    np.testing.assert_allclose(got, expected)
+
+
+@pytest.mark.parametrize("nu", (1, 3, 10))
+def test_qgaussian_cdf_vs_t(nu):
+    x = np.linspace(-5, 5)
+    q, sigma = q_sigma(nu, 1)
+
+    expected = t.cdf(x, nu, 0, 1)
+    got = qgaussian.cdf(x, q, 0, sigma)
 
     np.testing.assert_allclose(got, expected)
