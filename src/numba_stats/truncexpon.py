e8d84a20736cd740cebd1338012e83477817f0cd
Hans Dembinski <hans.dembinski@gmail.com>
2021-10-14 17:48:15 +0200

    add truncexpon and tests

---
+++ src/numba_stats/truncexpon.py
@@ -0,0 +1,64 @@
+import numba as nb
+import numpy as np
+from math import expm1, log1p
+
+_signatures = [
+    nb.float32(nb.float32, nb.float32, nb.float32, nb.float32, nb.float32),
+    nb.float64(nb.float64, nb.float64, nb.float64, nb.float64, nb.float64),
+]
+
+
+@nb.njit
+def _cdf(z):
+    return -expm1(-z)
+
+
+@nb.vectorize(_signatures, cache=True)
+def pdf(x, xmin, xmax, mu, sigma):
+    """
+    Return probability density of exponential distribution.
+    """
+    if x < xmin:
+        return 0.0
+    elif x > xmax:
+        return 0.0
+    sigma_inv = 1 / sigma
+    z = (x - mu) * sigma_inv
+    zmin = (xmin - mu) * sigma_inv
+    zmax = (xmax - mu) * sigma_inv
+    return np.exp(-z) * sigma_inv / (_cdf(zmax) - _cdf(zmin))
+
+
+@nb.vectorize(_signatures, cache=True)
+def cdf(x, xmin, xmax, mu, sigma):
+    """
+    Evaluate cumulative distribution function of exponential distribution.
+    """
+    if x < xmin:
+        return 0.0
+    elif x > xmax:
+        return 1.0
+    sigma_inv = 1 / sigma
+    z = (x - mu) * sigma_inv
+    p = _cdf(z)
+    zmin = (xmin - mu) * sigma_inv
+    zmax = (xmax - mu) * sigma_inv
+    pmin = _cdf(zmin)
+    pmax = _cdf(zmax)
+    return (p - pmin) / (pmax - pmin)
+
+
+@nb.vectorize(_signatures, cache=True)
+def ppf(p, xmin, xmax, mu, sigma):
+    """
+    Return quantile of exponential distribution for given probability.
+    """
+    sigma_inv = 1 / sigma
+    zmin = (xmin - mu) * sigma_inv
+    zmax = (xmax - mu) * sigma_inv
+    pmin = _cdf(zmin)
+    pmax = _cdf(zmax)
+    pstar = p * (pmax - pmin) + pmin
+    z = -log1p(-pstar)
+    x = z * sigma + mu
+    return x
---
+++ tests/test_truncexpon.py
@@ -0,0 +1,43 @@
+import numpy as np
+from numba_stats import truncexpon, expon
+
+
+def test_pdf():
+    x = np.linspace(-1, 5, 10)
+    xmin = 1
+    xmax = 4
+    mu = 2
+    sigma = 3
+    got = truncexpon.pdf(x, xmin, xmax, mu, sigma)
+    expected = expon.pdf(x, mu, sigma) / (
+        expon.cdf(xmax, mu, sigma) - expon.cdf(xmin, mu, sigma)
+    )
+    expected[x < xmin] = 0
+    expected[x > xmax] = 0
+    np.testing.assert_allclose(got, expected)
+
+
+def test_cdf():
+    x = np.linspace(-1, 5, 10)
+    xmin = 1
+    xmax = 4
+    mu = 1
+    sigma = 3
+    got = truncexpon.cdf(x, xmin, xmax, mu, sigma)
+    expected = (expon.cdf(x, mu, sigma) - expon.cdf(xmin, mu, sigma)) / (
+        expon.cdf(xmax, mu, sigma) - expon.cdf(xmin, mu, sigma)
+    )
+    expected[x < xmin] = 0
+    expected[x > xmax] = 1
+    np.testing.assert_allclose(got, expected)
+
+
+def test_ppf():
+    expected = np.linspace(0, 1, 10)
+    xmin = 1
+    xmax = 4
+    mu = 2
+    sigma = 3
+    x = truncexpon.ppf(expected, xmin, xmax, mu, sigma)
+    got = truncexpon.cdf(x, xmin, xmax, mu, sigma)
+    np.testing.assert_allclose(got, expected, atol=1e-14)
