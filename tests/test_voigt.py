6ddf5eab570e7e83ce7a5012c8a7cd8584ae225f
Hans Dembinski <hans.dembinski@gmail.com>
2021-10-14 18:02:51 +0200

    fix voigt error

---
+++ tests/test_voigt.py
@@ -0,0 +1,14 @@
+from scipy.special import voigt_profile
+from numba_stats import voigt
+import numpy as np
+
+
+def test_voigt_pdf():
+    gamma = 2
+    sigma = 1
+    mu = -1
+    x = np.linspace(-5, 5, 10)
+    got = voigt.pdf(x, gamma, mu, sigma)
+    # note that sigma comes before gamma in scipy
+    expected = voigt_profile(x - mu, sigma, gamma)
+    np.testing.assert_allclose(got, expected)
