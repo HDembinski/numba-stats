0d607a31e366b16b59fe0287be52c23b94081059
mhoene <57394584+mhoene@users.noreply.github.com>
2022-02-21 13:25:46 +0100

    Fix NumbaWarning: Compilation is falling back to object mode (#34)
    * Fix NumbaWarning: Compilation falling back to object mode
    * move shared code into private function
    * make code more consistent with norm
    
    Co-authored-by: Hans Dembinski <hans.dembinski@gmail.com>

--- src/numba_stats/lognorm.py
+++ src/numba_stats/lognorm.py
@@ -3,6 +3,16 @@ import numpy as np
 from .norm import _cdf, _ppf
 
 
+@nb.njit(cache=True)
+def _logpdf(x, s, loc, scale):
+    z = (x - loc) / scale
+    if z <= 0:
+        return -np.inf
+    c = np.sqrt(2 * np.pi)
+    log_pdf = -0.5 * np.log(z) ** 2 / s ** 2 - np.log(s * z * c)
+    return log_pdf - np.log(scale)
+
+
 _signatures = [
     nb.float32(nb.float32, nb.float32, nb.float32, nb.float32),
     nb.float64(nb.float64, nb.float64, nb.float64, nb.float64),
@@ -10,24 +20,19 @@ _signatures = [
 
 
 @nb.vectorize(_signatures, cache=True)
-def pdf(x, s, loc, scale):
-    """
-    Return probability density of lognormal distribution.
-    """
-    return np.exp(logpdf(x, s, loc, scale))
-
-
-@nb.vectorize(_signatures, cache=True)
 def logpdf(x, s, loc, scale):
     """
     Return log of probability density of lognormal distribution.
     """
-    z = (x - loc) / scale
-    if z <= 0:
-        return -np.inf
-    c = np.sqrt(2 * np.pi)
-    log_pdf = -0.5 * np.log(z) ** 2 / s ** 2 - np.log(s * z * c)
-    return log_pdf - np.log(scale)
+    return _logpdf(x, s, loc, scale)
+
+
+@nb.vectorize(_signatures, cache=True)
+def pdf(x, s, loc, scale):
+    """
+    Return probability density of lognormal distribution.
+    """
+    return np.exp(_logpdf(x, s, loc, scale))
 
 
 @nb.vectorize(_signatures, cache=True)
@@ -41,7 +46,7 @@ def cdf(x, s, loc, scale):
     return _cdf(np.log(z) / s)
 
 
-@nb.vectorize(_signatures)
+@nb.vectorize(_signatures)  # cannot be cached because of call to _ppf
 def ppf(p, s, loc, scale):
     """
     Return quantile of lognormal distribution for given probability.
