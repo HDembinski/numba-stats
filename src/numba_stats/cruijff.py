d5369b509d99199207a525cb5f1b4d487b637edc
amanmdesai <amanmukeshdesai@gmail.com>
2023-04-01 10:23:11 +0530

    add cruijff function

---
+++ src/numba_stats/cruijff.py
@@ -0,0 +1,47 @@
+"""
+Cruijff distribution.
+
+See For example: https://arxiv.org/abs/1005.4087
+"""
+
+from ._util import _jit, _generate_wrappers, _prange
+import numpy as np
+
+_doc_par = """
+x : ArrayLike
+    Random variate.
+mean : float
+    mean of the distribution
+sigma_left : float
+    left width
+sigma_right: float
+    right width
+alpha_left: float
+    left tail parameter
+alpha_right: float
+    right tail parameter
+"""
+
+
+@_jit(-4)
+def _logpdf_gen(x, mean, sigma, alpha):
+    return -((x - mean) ** 2) / (2 * sigma * sigma + alpha * (x - mean) ** 2)
+
+
+@_jit(5)
+def _logpdf(x, mean, sigma_left, sigma_right, alpha_left, alpha_right):
+    r = np.empty_like(x)
+    for i in _prange(len(x)):
+        if x[i] < mean:
+            r[i] = _logpdf_gen(x[i], mean, sigma_left, alpha_left)
+        elif x[i] > mean:
+            r[i] = _logpdf_gen(x[i], mean, sigma_right, alpha_right)
+    return r
+
+
+@_jit(5)
+def _pdf(x, mean, sigma_left, sigma_right, alpha_left, alpha_right):
+    return np.exp(_logpdf(x, mean, sigma_left, sigma_right, alpha_left, alpha_right))
+
+
+_generate_wrappers(globals())
