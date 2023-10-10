0d66f586266f0f7c43d906023a1de8efe75cf5d8
Hans Dembinski <hans.dembinski@gmail.com>
2023-10-10 18:09:24 +0200

    nicer

--- bench/test_stats.py
+++ bench/test_stats.py
@@ -147,15 +147,14 @@ def test_speed_bernstein_density(benchmark, lib, n):
         def method(x, beta, xmin, xmax):
             return BPoly(np.array(beta)[:, np.newaxis], [xmin, xmax])(x)
 
+    elif lib == "ours:parallel,fastmath":
+
+        @nb.njit(parallel=True, fastmath=True)
+        def method(x, beta, xmin, xmax):
+            return bernstein.density(x, beta, xmin, xmax)
+
     else:
-        if lib == "ours:parallel,fastmath":
-
-            @nb.njit(parallel=True, fastmath=True)
-            def method(x, beta, xmin, xmax):
-                return bernstein.density(x, beta, xmin, xmax)
-
-        else:
-            method = bernstein.density
+        method = bernstein.density
 
     # warm-up JIT
     method(x, beta, xmin, xmax)
