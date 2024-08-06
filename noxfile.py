e5f043b331565537cf9dff6798abcb5db7074b28
Hans Dembinski <hans.dembinski@gmail.com>
2024-08-06 16:04:21 +0200

    fix

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,5 @@
 """
-Noxfile for iminuit.
+Noxfile to orchestrate tests and computing coverage.
 
 Pass extra arguments to pytest after --
 """
@@ -21,33 +21,23 @@ PYPROJECT = nox.project.load_toml("pyproject.toml")
 MINIMUM_PYTHON = PYPROJECT["project"]["requires-python"].strip(">=")
 LATEST_PYTHON = str(python_releases.latest())
 
-nox.options.sessions = ["test", "mintest", "maxtest"]
+nox.options.sessions = ["test", "maxtest"]
+
+# running in parallel with pytest-xdist does not make the tests faster
 
 
 @nox.session(reuse_venv=True)
 def test(session: nox.Session) -> None:
     """Run all tests."""
-    # running in parallel with pytest-xdist crashes ROOT
     session.install("-e.[test]")
-    extra_args = session.posargs if session.posargs else ("-n=auto",)
-    session.run("pytest", *extra_args)
-
-
-# broken: this tries to compile dependencies
-@nox.session(python=MINIMUM_PYTHON, venv_backend="uv", reuse_venv=True)
-def mintest(session: nox.Session) -> None:
-    """Run tests on the minimum python version."""
-    session.install("-ve.[test]", "--only-binary=1", "--resolution=lowest-direct")
-    extra_args = session.posargs if session.posargs else ("-n=auto",)
-    session.run("pytest", *extra_args)
+    session.run("pytest", *session.posargs)
 
 
 @nox.session(python=LATEST_PYTHON, reuse_venv=True)
 def maxtest(session: nox.Session) -> None:
     """Run the unit and regular tests."""
-    session.install("-ve.[test]", "--only-binary=1")
-    extra_args = session.posargs if session.posargs else ("-n=auto",)
-    session.run("pytest", *extra_args, env=ENV)
+    session.install("-e.[test]")
+    session.run("pytest", *session.posargs, env=ENV)
 
 
 # Python-3.12 provides coverage info faster
@@ -56,5 +46,5 @@ def cov(session: nox.Session) -> None:
     """Run covage and place in 'htmlcov' directory."""
     session.install("-e.[test]")
     session.run("coverage", "run", "-m", "pytest", env=ENV)
-    session.run("coverage", "html", "-d", "build/htmlcov")
+    session.run("coverage", "html", "-d", "htmlcov")
     session.run("coverage", "report", "-m")
