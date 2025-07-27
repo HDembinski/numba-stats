bbcf31ab2b6122e6cbaf7ad64b4972cb11f1c5d1
Hans Dembinski <hans.dembinski@gmail.com>
2025-07-27 16:56:18 +0200

    fix comment

--- src/numba_stats/crystalball_ex.py
+++ src/numba_stats/crystalball_ex.py
@@ -6,17 +6,17 @@ an asymmetric normal distribution with power-law tails. Furthermore, the scale
 is allowed to vary between the left and the right side of the peak. There is no
 discontinuity at the maximum or elsewhere.
 
-The generalized Crystal Ball distribution is often used empirically to model bell curve
-that has heavier tails than a normal distribution. For a symmetric distribution, the
-superior but less well-known alternative is the Student's t-distribution. A superior
-asymmetric form also exists, called the non-central Student's t-distribution - which is
-not implemented in numba-stats yet.
+The generalized Crystal Ball distribution is often used to empirically model a 
+bell curve with heavier tails than a normal distribution. For a symmetric
+distribution, the superior but less well-known alternative is the Student's
+t-distribution. A superior asymmetric form also exists, called the non-central
+Student's t-distribution - which is not implemented in numba-stats yet.
 
 The Student's t distribution is superior, because it can be derived from an actual
-statistical process, while the ad hoc Crystal Ball stitches unrelated
-distributions together, a better name would be the 'Frankenstein distribution'. The
-construction makes it numerically very difficult to fit, which has caused many a
-grievance among physicists, which would have been avoided by using Student's t.
+statistical process, while the ad hoc Crystal Ball stitches unrelated distributions
+together, a better name would be the 'Frankenstein distribution'. The construction
+makes it numerically very difficult to fit, which has caused many a grievance among
+practitioners, which could have been avoided by using the more stable Student's t.
 """
 
 from .crystalball import (
